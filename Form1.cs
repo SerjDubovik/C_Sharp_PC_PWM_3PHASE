4134cea893ab82a3a982855a2ac6128ac2379e56
Sergey <mail@pufr.ru>
2021-03-23 23:28:58 +0300

    коммит перед переходом.

---
+++ Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+using System.Linq;
+using Modbus.Device;    															        // for modbus master
+using System.IO.Ports;  															        // for serial port
+using System.Threading;
+using System.Text;
+
+
+namespace C_Sharp_PC_PWM_3PHASE
+{
+    public partial class Form1 : Form
+    {
+
+
+		public List<string> existing_port_list = new List<string>() { "Не использовать" };  // лист доступных в системе СОМ портов в данный момент
+
+		public string nomber_net_buf;                                                       // номер устройства в сети
+		public string speed_net_buf;                                                        // скорость передачи по выбранному каналу модбас
+		public string stop_bit_net_buf;                                                     // кол-во стоп бит
+		public string quantity_bit_buf;                                                     // кол-во бит в посылке
+		public string parity_net_buf;                                                   // выбранная проверка в посылке
+		public string post_buf;                                                             // СОМ порт для канала связи
+
+		public bool autoconnect_buf;                                                    // буль если тру, то прога самоподключается при старте
+
+		public static ushort[] modbus_mass = new ushort[200];                               // массив для взаимодействия с потоком обработки модбас
+
+		public Property_Form property_Form;                                                 // Объявляем класс с формой настроек				
+
+		public Class_visu_prop_grid_modbus visu_MyClass_modbus = new Class_visu_prop_grid_modbus(); // создаём экземпляр класса для отображения в сетке свойств настроек modbus
+
+		public Serializable_Class serializable_Class;
+
+		public SerialPort serialPort = new SerialPort();                                           // create a new SerialPort object with default settings.			
+
+		public ModBus_var modBus_var = new ModBus_var();                                    // создаём экземпляр класса для передачи в поток
+		public Thread Thread_Modbus = new Thread(new ParameterizedThreadStart(Modbus_func));       // Вот так передаём в созданный поток класс
+
+		public static ModbusSerialMaster master;
+
+
+		public Form1()
+        {
+            InitializeComponent();
+
+			serializable_Class = new Serializable_Class(            // инициируем класс для работы с сериализером
+								nomber_net_buf,                     // номер устройства в сети
+								speed_net_buf,                      // скорость передачи по выбранному каналу модбас
+								stop_bit_net_buf,                   // кол-во стоп бит
+								quantity_bit_buf,                   // кол-во бит в посылке
+								parity_net_buf,                     // выбранная проверка в посылке
+								post_buf,
+								autoconnect_buf);
+
+			Deserialization("default.dat");                         // десериализуем ранее сохранённые настройки
+
+
+
+
+			property_Form = new Property_Form(this);
+			property_Form.Tag = this;
+
+
+			existing_port_list.AddRange(SerialPort.GetPortNames());                     // узнаём какие порты активны сейчс и заносим их в лист
+
+			checkBox1.Checked = true;													// при старте выставлена галка автономной работы
+
+			if (checkBox2.Checked)
+			{
+				label_state_rele.Text = "Реле замкнуто";
+				label_state_rele.BackColor = Color.FromArgb(128, 255, 128);
+			}
+			else
+			{
+				label_state_rele.Text = "Реле разомкнуто";
+				label_state_rele.BackColor = Color.FromArgb(255, 128, 128);
+			}
+
+
+		}
+
+        private void ToolStripMenuItem_exit_Click(object sender, EventArgs e)
+        {
+			Serialization("default.dat");
+			Thread_Modbus.Abort();                                                          // заставляет прервать поток обработки модбас	
+			Close();
+		}
+
+        private void ToolStripMenuItem_transport_btn_Click(object sender, EventArgs e)
+        {
+			property_Form.tabControl1.SelectTab(0);                                         // показываем окно с первой вкладки
+			property_Form.Show();                                                           // показываем окно	
+		}
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+			Serialization("default.dat");
+			Thread_Modbus.Abort();                                                      // заставляет прервать поток обработки модбас
+		}
+
+        private void timer_for_Displ_Tick(object sender, EventArgs e)
+        {			
+			toolStripStatusLabel7.Text = Convert.ToString(modBus_var.mb_mass[8]);       // тестовый счётчик в потоке модбаса в плате. в строке состояния.
+
+
+			A0.Text = Convert.ToString(modBus_var.mb_mass[13]);							// GPIOA.0		Isens - вход с токового датчика
+			A4.Text = Convert.ToString(modBus_var.mb_mass[14]);							// GPIOA.4		Uzpt - вход, напряжение звена постоянного тока
+			A5.Text = Convert.ToString(modBus_var.mb_mass[15]);							// GPIOA.5		Un 	- выходное напряжение. обратная связь
+			A6.Text = Convert.ToString(modBus_var.mb_mass[16]);							// GPIOA.6		Ibreak - ток отсечки. защитная функция.
+
+			M9.Text = Convert.ToString(modBus_var.mb_mass[9]/1000.0);					// Isens * K_Isens
+			M10.Text = Convert.ToString(modBus_var.mb_mass[10]);						// Uzpt * K_Uzpt
+			M11.Text = Convert.ToString(modBus_var.mb_mass[11]/1000.0);                 // Un * K_Un
+			M12.Text = Convert.ToString(modBus_var.mb_mass[12]/100.0);					// Ibreak * K_Ibreak
+			
+			M21.Text = Convert.ToString(modBus_var.mb_mass[21]);                        // Порог срабатыания реле ЗПТ
+			M23.Text = Convert.ToString(modBus_var.mb_mass[23]);                        // Текущая частота(кГц).
+			M25.Text = Convert.ToString(modBus_var.mb_mass[25]);                        // Текущий дедтайм (ед).
+			M27.Text = Convert.ToString(modBus_var.mb_mass[27]);                        // Текущий ШИМ.
+			M30.Text = Convert.ToString(modBus_var.mb_mass[30]);                        // Текущее токоограничение (мА).
+			M32.Text = Convert.ToString(modBus_var.mb_mass[32]);                        // Температура платы управления (С).
+			M33.Text = Convert.ToString(modBus_var.mb_mass[33]);                        // Порог платы управления (С).
+			M35.Text = Convert.ToString(modBus_var.mb_mass[35]);                        // Температура радиатора (С).
+			M36.Text = Convert.ToString(modBus_var.mb_mass[33]);                        // Порог радиатора (С).
+			M50.Text = Convert.ToString(modBus_var.mb_mass[50]);                        // Текущая скважность.
+			M52.Text = Convert.ToString(modBus_var.mb_mass[52]);                        // Текущая скважность Buck-конвертора.
+
+			label_control.Text = Convert.ToString(modBus_var.mb_mass[1]);				// слово управления
+			label_telemetry.Text = Convert.ToString(modBus_var.mb_mass[0]);             // состояние системы
+
+
+
+
+			if ((modBus_var.mb_mass[0] &= 0x4000) != 0)//
+			{
+				label_state_rele.Text = "Реле замкнуто";
+				label_state_rele.BackColor = Color.FromArgb(128, 255, 128);
+				//checkBox2.Checked = true;
+			}
+			else
+			{
+				label_state_rele.Text = "Реле разомкнуто";
+				label_state_rele.BackColor = Color.FromArgb(255, 128, 128);
+				//checkBox2.Checked = false;
+			}
+
+		}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)			// принудительно замкнуть реле
+        {
+			if (checkBox2.Checked)													// замкнуть реле
+			{
+				modBus_var.mb_mass[1] &= 0xDFFF;
+				modBus_var.mb_mass[1] |= 0x4000;
+			}
+			else																	// разомкнуть реле
+			{
+				modBus_var.mb_mass[1] &= 0xBFFF;
+				modBus_var.mb_mass[1] |= 0x2000;				
+			}
+		}
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)			// автономная работа
+        {
+			if (checkBox1.Checked)
+			{
+				modBus_var.mb_mass[1] |= 0x1000;									// установим 12 бит, автономная работа зпт
+
+				checkBox2.Hide();
+			}
+			else
+			{
+				modBus_var.mb_mass[1] &= 0xEFFF;                                    // сбросим 12 бит, переход на ручной режим
+
+				if ((modBus_var.mb_mass[0] &= 0x4000) != 0)							// если в момент перехода на ручное управление, реле было замкнуто, выставим галку.
+				{
+					checkBox2.Checked = true;
+				}
+				else
+				{
+					checkBox2.Checked = false;
+				}
+
+				checkBox2.Show();
+			}
+		}
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)			// включить/выключить шим
+        {
+			if (checkBox4.Checked)                                                  // 
+			{
+
+			}
+			else                                                                    // 
+			{
+
+			}
+		}
+
+
+        private void button_set_ZPT_threshold_Click(object sender, EventArgs e)		// кнопка задания порога срабатывания зпт.
+        {
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[22] = Convert.ToUInt16(numericUpDown1.Value);   // 
+				modBus_var.mb_mass[2] = 10;
+			}
+			else 
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+			
+		}
+
+        private void button_set_freq_threshold_Click(object sender, EventArgs e)	// задает частоту следования импульсов
+        {
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[24] = Convert.ToUInt16(numericUpDown3.Value);   // 
+				modBus_var.mb_mass[2] = 11;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+        private void button_set_dedtime_Click(object sender, EventArgs e)			// задаёт дедтайм
+        {
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[26] = Convert.ToUInt16(numericUpDown6.Value);   // 
+				modBus_var.mb_mass[2] = 12;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+        private void button_set_Uout_Click(object sender, EventArgs e)				// задать выходное напряжение
+        {
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[29] = Convert.ToUInt16(numericUpDown4.Value);   // 
+				modBus_var.mb_mass[2] = 13;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+
+        private void button_set_Iout_Click(object sender, EventArgs e)				// задать токоограничение выхода
+        {
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[31] = Convert.ToUInt16(numericUpDown5.Value);   // 
+				modBus_var.mb_mass[2] = 14;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+        private void button_set_T_plate_threshold_Click(object sender, EventArgs e)	// задаёт порог срабатывания по температуре платы
+        {
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[34] = Convert.ToUInt16(numericUpDown7.Value);   // 
+				modBus_var.mb_mass[2] = 15;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+        private void button_set_T_heater_threshold_Click(object sender, EventArgs e) // задаёт порог срабатывания по температурерадиатора
+		{
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[37] = Convert.ToUInt16(numericUpDown8.Value);   // 
+				modBus_var.mb_mass[2] = 16;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+		private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+		{
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[28] = Convert.ToUInt16(numericUpDown2.Value);   // 
+				modBus_var.mb_mass[2] = 17;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+		private void checkBox3_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox3.Checked)                                                  // замкнуть реле
+			{
+				modBus_var.mb_mass[1] |= 0x8000;
+			}
+			else                                                                    // разомкнуть реле
+			{
+				modBus_var.mb_mass[1] &= 0x7FFF;
+			}
+		}
+
+		private void numericUpDown16_ValueChanged(object sender, EventArgs e)
+		{
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[51] = Convert.ToUInt16(numericUpDown16.Value);   // 
+				modBus_var.mb_mass[2] = 18;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+
+		private void numericUpDown17_ValueChanged(object sender, EventArgs e)
+		{
+			if (modBus_var.mb_mass[2] == 0)
+			{
+				modBus_var.mb_mass[53] = Convert.ToUInt16(numericUpDown17.Value);   // 
+				modBus_var.mb_mass[2] = 19;
+			}
+			else
+			{
+				MessageBox.Show("Не торопись. Протокол за тобой не поспевает.");
+			}
+		}
+	}
+
+
+
+
+
+
+
+	public class ModBus_var
+	{
+		public byte adrr_dev_in;
+		public ushort adrr_var_in;
+		public ushort adrr_var_out;
+		public ushort lenght_in;
+
+		public ushort buton;
+
+		public ushort flag_connect;
+		public ushort flag_read_param;
+		public ushort flag_write_param;
+
+		public ushort[] mb_mass;
+
+
+		public ModBus_var()
+		{
+			mb_mass = new ushort[200];
+
+		}
+
+	}
+}
---
+++ Modbus_func.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+using Modbus.Device;
+
+
+namespace C_Sharp_PC_PWM_3PHASE
+{
+	public partial class Form1
+    {		
+		
+		
+		public void connect_modbus()
+		{									
+					
+			serialPort.PortName 	= post_buf;									// СОМ порт для каждого канала
+			serialPort.BaudRate 	= Convert.ToInt32(speed_net_buf);			// скорость передачи по выбранному каналу модбас
+			serialPort.DataBits 	= Convert.ToInt32(quantity_bit_buf);		// кол-во бит в посылке
+
+			
+			switch (parity_net_buf)												// проверка на чётность
+			{
+				case "Even": serialPort.Parity 		= 	Parity.Even;	break;
+				case "Mark": serialPort.Parity 		= 	Parity.Mark;	break;
+				case "None": serialPort.Parity 		= 	Parity.None;	break;
+				case "Odd": serialPort.Parity 		= 	Parity.Odd;		break;
+				case "Space": serialPort.Parity 	= 	Parity.Space;	break;			
+			}
+			
+			
+			
+			switch (stop_bit_net_buf)											// кол-во стоп бит
+			{
+				case "None":  serialPort.StopBits				=	StopBits.None;	break;
+				case "One":  serialPort.StopBits				=	StopBits.One;	break;
+				case "OnePointFive":  serialPort.StopBits		=	StopBits.OnePointFive;	break;
+				case "Two":  serialPort.StopBits				=	StopBits.Two;	break;								
+										
+			}
+
+            try
+            {
+                serialPort.Open();
+                master = ModbusSerialMaster.CreateRtu(serialPort);
+                master.Transport.Retries = 0;       // don't have to do retries
+                master.Transport.ReadTimeout = 50; // milliseconds
+
+                toolStripStatusLabel2.Text = DateTime.Now.ToString() + " " + serialPort.PortName + " Открыт ";              // выводим сообщение в строку состояния
+                
+        
+
+            }
+
+            catch (Exception)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                toolStripStatusLabel2.Text = DateTime.Now.ToString() + " " + serialPort.PortName + " Не удалось открыть ";  // выводим сообщение в строку состояния
+            }
+
+
+            try
+            {
+            	Thread_Modbus.Start(modBus_var);                                 // запуск процесса обработки ком порта
+                toolStripStatusLabel4.Text = " Поток запущен ";                  // выводим сообщение в строку состояния
+
+            }
+            catch (Exception)
+            {
+                toolStripStatusLabel4.Text = " Поток не запущен ";               // выводим сообщение в строку состояния
+            }
+            
+		}
+		
+		
+		
+		
+		
+		
+		public static void Modbus_func(object obj)                                     // метод потока. для работы с ком портом
+		{
+
+			ModBus_var modBus_var = (ModBus_var)obj;
+			
+			bool enable_loop = true;
+						
+
+			
+            while (enable_loop)														// не забываем за бесконечный цикл в потоках =)
+            {    
+
+            	if(enable_loop == false)
+            	{
+            		break;
+            	}
+            	
+    	        try
+                {
+					
+                    ushort[] register;
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 0, 2);           //  телеметрия состояния контроллера
+					modBus_var.mb_mass[0] = register[0];
+
+					// 15	-	1 если включился и ждёт команды от пользователя.
+					// 14	-	
+					// 13	-	
+					// 12	-	
+					// 11	-	
+					// 10	-	
+					// 9	-	
+					// 8	-	
+
+					// 7	-	
+					// 6	-	
+					// 5	-	
+					// 4	-	
+					// 3	-	
+					// 2	-	
+					// 1	-	
+					// 0	-	
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 8, 2);           // счётчик для проверки связи
+                    modBus_var.mb_mass[8] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 9, 2);           // GPIOA.0		Isens - вход с токового датчика
+					modBus_var.mb_mass[9] = register[0];
+					
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 10, 2);          // GPIOA.4		Uzpt - вход, напряжение звена постоянного тока
+					modBus_var.mb_mass[10] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 11, 2);          // GPIOA.5		Un 	- выходное напряжение. обратная связь
+					modBus_var.mb_mass[11] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 12, 2);          // GPIOA.6		Ibreak - ток отсечки. защитная функция.
+					modBus_var.mb_mass[12] = register[0];
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 13, 2);          // GPIOA.0		Isens значение АЦП без пересчёта
+					modBus_var.mb_mass[13] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 14, 2);          // GPIOA.4		Uzpt значение АЦП без пересчёта
+					modBus_var.mb_mass[14] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 15, 2);          // GPIOA.5		Un значение АЦП без пересчёта
+					modBus_var.mb_mass[15] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 16, 2);          // GPIOA.6		Ibreak значение АЦП без пересчёта
+					modBus_var.mb_mass[16] = register[0];
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 17, 2);          // Коэффициент для пересчёта из АЦП в Амперы тока выхода.Телеметрия, мастер только читает.
+					modBus_var.mb_mass[17] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 18, 2);          // Коэффициент для пересчёта из АЦП в Вольт ЗПТ. Телеметрия, мастер только читает.
+					modBus_var.mb_mass[18] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 19, 2);          // Коэффициент для пересчёта из АЦП в Вольт выхода. Телеметрия, мастер только читает.
+					modBus_var.mb_mass[19] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 20, 2);          // Коэффициент для пересчёта из АЦП в Амперы на ключе.Телеметрия, мастер только читает.
+					modBus_var.mb_mass[20] = register[0];
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 21, 2);          // Нижнее пороговое значение напряжения ЗПТ. Порог срабатывания реле ЗПТ. Телеметрия, мастер не меняет это значение
+					modBus_var.mb_mass[21] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 22, modBus_var.mb_mass[22]); // Нижнее пороговое значение напряжения ЗПТ. Порог срабатывания реле ЗПТ. Задание мастером, слейв не менят это значение
+
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 23, 2);          // Текущая частота(кГц).
+					modBus_var.mb_mass[23] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 24, modBus_var.mb_mass[24]);	// Задать частоту(кГц).
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 25, 2);          // Текущий дедтайм(ед).
+					modBus_var.mb_mass[25] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 26, modBus_var.mb_mass[26]); // Задать дедтайм(ед).
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 27, 2);          // Текуший ШИМ.
+					modBus_var.mb_mass[27] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 28, modBus_var.mb_mass[28]); // Едениц задания ШИМ.
+
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 29, modBus_var.mb_mass[29]); // Задать выходное напряжение(кВ).
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 30, 2);          // Текущее токоограничение(мА).
+					modBus_var.mb_mass[30] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 31, modBus_var.mb_mass[31]); // Задать токоограничение(мА):
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 32, 2);          // Температура платы управления(С).
+					modBus_var.mb_mass[32] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 33, 2);          // Порог платы управления(С).
+					modBus_var.mb_mass[33] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 34, modBus_var.mb_mass[34]); // Задать порог платы(С).
+
+
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 35, 2);          // Температура радиатора(С).
+					modBus_var.mb_mass[35] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 36, 2);          // Порог радиатора(С).
+					modBus_var.mb_mass[36] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 37, modBus_var.mb_mass[37]);	// Задать порог радиатора(С).
+
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 38, modBus_var.mb_mass[38]); // Коэффициент для пересчёта из АЦП в Амперы тока выхода.Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 39, modBus_var.mb_mass[39]); // Коэффициент для пересчёта из АЦП в Вольт ЗПТ. Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 40, modBus_var.mb_mass[40]); // Коэффициент для пересчёта из АЦП в Вольт выхода. Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 41, modBus_var.mb_mass[41]); // Коэффициент для пересчёта из АЦП в Амперы на ключе.Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 42, modBus_var.mb_mass[42]); // Делитель для преобразования из целого в дробное.Для тока выхода.Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 43, modBus_var.mb_mass[43]); // Делитель для преобразования из целого в дробное.Для напряжения ЗПТ.Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 44, modBus_var.mb_mass[44]); // Делитель для преобразования из целого в дробное.Для напряжения выхода.Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 45, modBus_var.mb_mass[45]); // Делитель для преобразования из целого в дробное.Для тока отсечки.Мастер задаёт это значение. Код для подтверждения(запись в адрес 2):
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 46, 2);          // Делитель для преобразования из целого в дробное.Для тока выхода.Мастер читает это значение и формирует дробное число для отображения.
+					modBus_var.mb_mass[46] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 47, 2);          // Делитель для преобразования из целого в дробное.Для напряжения ЗПТ.Мастер читает это значение и формирует дробное число для отображения.
+					modBus_var.mb_mass[47] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 48, 2);          // Делитель для преобразования из целого в дробное.Для напряжения выхода.Мастер читает это значение и формирует дробное число для отображения.
+					modBus_var.mb_mass[48] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 49, 2);          // Делитель для преобразования из целого в дробное.Для тока отсечки.Мастер читает это значение и формирует дробное число для отображения.
+					modBus_var.mb_mass[49] = register[0];
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 50, 2);          // Текущая скважность.
+					modBus_var.mb_mass[50] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 51, modBus_var.mb_mass[51]); // Едениц задания ШИМ.
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 52, 2);          // Текущая скважность в Buck-конверторе.
+					modBus_var.mb_mass[52] = register[0];
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 53, modBus_var.mb_mass[53]); // Едениц задания ШИМ в Buck-конверторе.
+
+
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 1, modBus_var.mb_mass[1]);   //  запись управляющего слова
+					master.WriteSingleRegister(modBus_var.adrr_dev_in, 2, modBus_var.mb_mass[2]);
+
+
+					register = master.ReadHoldingRegisters(modBus_var.adrr_dev_in, 3, 2);           // слейв пишет в этот адрес код команды слейву
+					modBus_var.mb_mass[3] = register[0];
+
+					if (modBus_var.mb_mass[3] == 1)
+					{
+						modBus_var.mb_mass[2] = 0;
+					}
+
+				}
+
+				catch 
+				{
+
+				}
+            	               	
+            }
+			
+        }
+		
+	}
+	
+}
